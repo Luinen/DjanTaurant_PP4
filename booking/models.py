ba341f36f20d2939887f4008ec3aed16ee416dc6
Luinen <110351692+Luinen@users.noreply.github.com>
2023-04-23 13:00:58 +0000

    add User class to models

--- booking/models.py
+++ booking/models.py
@@ -1,8 +1,24 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django import forms
+from datetime import datetime    
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 # Create your models here.
 
-#class Booking(models.Model):
-    
+class User(models.Model):
+    # user_id
+    user_firstname = models.CharField(max_length=50)
+    user_lastname = models.CharField(max_length=50)
+    user_username = models.CharField(max_length=100)
+    user_email = models.EmailField('email address', max_length=100, unique=True)
+    user_password = models.CharField(max_length=50)
+    date_joined = models.DateTimeField(default=timezone.now)
+    is_staff = models.BooleanField(default=False)
+
+
+#class UserForm(forms.ModelForm):
+#    password = forms.CharField(widget=forms.PasswordInput)
+#    class Meta:
+#        model = User
+
--- booking/urls.py
+++ booking/urls.py
@@ -1,6 +1,6 @@
 from .import views
 from django.urls import path
 
-urlpatterns = [
-    path('', , name= 'home')
-]
+#urlpatterns = [
+#    path('', , name= 'home')
+#]
