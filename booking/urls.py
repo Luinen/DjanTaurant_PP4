bd3e950b3f52fbffbd60c9fd27cb8bfdf3277c80
Luinen <lui.peto@gmail.com>
2023-05-05 18:08:08 +0000

    add form to reservation

--- booking/urls.py
+++ booking/urls.py
@@ -2,7 +2,8 @@ from .import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.mainPage, name= 'main'),
+    path('', views.mainPage, name='main'),
     path('reservation/', views.Reservation.as_view(), name="reservation"),
     path('menu/', views.Menu.as_view(), name="menu"),
+    path('reservation/makereservation/', views.bookingCreate, name="makereservation"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 from django.template import loader
-from booking.models import Food
+from booking.models import Food, Booking, Table
+from django import forms
 
 # Create your views here.
 
 
 def mainPage(request):
     response = loader.get_template('index.html')
-    food = Food.objects.all()
+    food = Food.objects.all()[:4]
     context = {
-        'foods' : food
+        'foods': food
     }
     return HttpResponse(response.render(context, request))
 
@@ -22,11 +23,31 @@ class Reservation(View):
     def get(self, request):
         return render(request, "reservation.html")
 
+
+class ReservationForm(forms.Form):
+    reservation_start = forms.DateTimeField(label="Start of Reservation")
+    guests = forms.IntegerField(label="Number of Guests")
+    
+
+
+def bookingCreate(request):
+    print("/////////////////////////////")
+    form = ReservationForm(request.POST)
+    print(form.is_valid())
+    if form.is_valid():
+        return HttpResponse("Thank You")
+    else:
+        return HttpResponse("No reservation made")
+     #   print(form.cleaned_data["reservation_start"])
+    #return HttpResponseRedirect("reservation/")
+
+
 class Menu(View):
 
     def get(self, request):
         food = Food.objects.all()
         context = {
-            'foods' : food
+            'foods': food
             }
         return render(request, "menu.html", context=context)
+
